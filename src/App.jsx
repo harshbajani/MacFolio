f7c68dc6bd4c1ab984d1cc1f4f677911615b9196
harshbajani <harshbajani7@gmail.com>
2025-11-25 11:15:59 +0530

    Add Finder, Image, and Text window components; update App layout and dependencies
    - Introduced new Finder, Image, and Text components for enhanced window functionality.
    - Updated App component to include the new windows alongside existing ones.
    - Added clsx dependency for conditional class management.
    - Created a location store using Zustand for managing active locations.
    - Enhanced exports in index.js to include the new components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Dock, Navbar, Welcome } from "@components";
-import { Resume, Safari, Terminal } from "@windows";
+import { Finder, Image, Resume, Safari, Terminal, Text } from "@windows";
 import gsap from "gsap";
 
 import { Draggable } from "gsap/Draggable";
@@ -14,6 +14,9 @@ const App = () => {
       <Terminal />
       <Safari />
       <Resume />
+      <Finder />
+      <Text />
+      <Image />
     </main>
   );
 };
